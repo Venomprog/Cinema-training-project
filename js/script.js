8a30e6b8ef1a08a6d4c78b005227d5a0d65cc0b9
Shaev Gleb <75539098+Venomprog@users.noreply.github.com>
2022-10-04 22:34:04 +0300

    create project

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+// const adv = document.querySelector('.promo__adv');
+const adv = document.getElementsByClassName('promo__adv');
+const genre = document.querySelector('.promo__genre');
+const bg = document.querySelector('.promo__bg');
+
+bg.style.cssText = 'background-image: url(img/bg.jpg)';
+genre.textContent = "Драма";
+adv[0].remove();
+
+
+
+
+
