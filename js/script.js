ea954c2b9266129431eec0d43552c57a7665ae5f
Gleb <glebshaevvv@mail.ru>
2023-02-11 21:46:13 +0300

    commetn

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     };
     
     const adv = document.querySelectorAll('.promo__adv img');
-    const poster = document.querySelector('.promo__bg'); //Получение объекта главного экрана фильма
+    const poster = document.querySelector('.promo__bg'); //Получение объекта главного экрана фильма!
     const genre = poster.querySelector('.promo__genre');
     const filmsList = document.querySelector('.promo__interactive-list'); //получение эл-ов со страницы!
     const addForm = document.querySelector('form.add');
@@ -50,40 +50,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
     renderMovies(movieDB.movies, filmsList);
     
-    // movieDB.movies.forEach(function(item, i){ //Перепишем код для чёткого создания элемента
-    //     filmsList.innerHTML += `
-    //     <li class="promo__interactive-item">${i + 1} ${item} 
-    //         <div class="delete"></div>
-    //     </li>
-    //     `;
-    // });
-    
-    //task 4 complete
-    //task 5 complete
-    
-    
-    // const typeClick = (e) => { //функция при евенте пишет цель и тип евента
-    //     console.log(e.target);
-    //     console.log(e.type);
-    // };
-    // document.addEventListener('click', typeClick); //Обработчик на документ, при клике вызывает функцию
-    
-    // let i = 0;
-    // const dlt = (event) =>{
-    //     i++;
-        
-    //     if ( i == 3){
-    //         filmsList.removeEventListener('click', dlt); //При клике 3 раза убирает слушателя события
-    //     }
-    //     console.log(i);
-    // };
-    
-    // filmsList.addEventListener('mouseenter', typeClick);
-    
-    // filmsList.addEventListener('click', (event, dlt) =>{
-    //     console.log('done'); //При клике на элементы списка фильмов выводит сообщение в консоль
-    // });
-    // Изучение DOM-элементов
+   
     
     addForm.addEventListener('submit', (event) => {
         event.preventDefault();
